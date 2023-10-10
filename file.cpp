6cf9aa5a6e546c2e95c2e85b89c1e1ec66011b63
Niels <91876336+NielsOuvrard@users.noreply.github.com>
2023-10-10 15:38:53 +0200

    🌟 cleaner header

--- file.cpp
+++ file.cpp
@@ -1,10 +1,10 @@
 /*
-**  d888b  d8888b.  .d8b.  d8888b. db   db  .d8b.  d8b   db  .d8b.  db      db    db .d8888. d888888b .d8888.
-** 88' Y8b 88  `8D d8' `8b 88  `8D 88   88 d8' `8b 888o  88 d8' `8b 88      `8b  d8' 88'  YP   `88'   88'  YP
-** 88      88oobY' 88ooo88 88oodD' 88ooo88 88ooo88 88V8o 88 88ooo88 88       `8bd8'  `8bo.      88    `8bo.
-** 88  ooo 88`8b   88~~~88 88~~~   88~~~88 88~~~88 88 V8o88 88~~~88 88         88      `Y8b.    88      `Y8b.
-** 88. ~8~ 88 `88. 88   88 88      88   88 88   88 88  V888 88   88 88booo.    88    db   8D   .88.   db   8D
-**  Y888P  88   YD YP   YP 88      YP   YP YP   YP VP   V8P YP   YP Y88888P    YP    `8888Y' Y888888P `8888Y'
+**    d888b  d8888b.  .d8b.  d8888b. db   db       .d8b.  d8b   db  .d8b.  db   db    db .d8888. d888888b .d8888.
+**   88' Y8b 88  `8D d8' `8b 88  `8D 88   88      d8' `8b 888o  88 d8' `8b 88   `8b  d8' 88'  YP   `88'   88'  YP
+**   88      88oobY' 88ooo88 88oodD' 88ooo88      88ooo88 88V8o 88 88ooo88 88    `8bd8'  `8bo.      88    `8bo.
+**   88  ooo 88`8b   88~~~88 88~~~   88~~~88      88~~~88 88 V8o88 88~~~88 88      88      `Y8b.    88      `Y8b.
+**   88. ~8~ 88 `88. 88   88 88      88   88      88   88 88  V888 88   88 88booo. 88    db   8D   .88.   db   8D
+**    Y888P  88   YD YP   YP 88      YP   YP      YP   YP VP   V8P YP   YP Y88888P YP    `8888Y' Y888888P `8888Y'
 **
 **
 */
@@ -21,8 +21,8 @@ void help(void)
     std::cout << "USAGE" << std::endl;
     std::cout << "\t./game_of_graphs [--links fr p1 p2 | --plots fr cr n]" << std::endl
               << std::endl;
-    std::cout << "DESCRIPTION" << std::endl;
 
+    std::cout << "DESCRIPTION" << std::endl;
     std::cout << "\tfr\tfile containing friendship relations between people" << std::endl;
     std::cout << "\tpi\tname of someone in the friendships file" << std::endl;
     std::cout << "\tcr\tfile containing conspiracies intentions" << std::endl;
